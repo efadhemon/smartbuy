06fc3a36699873a833d64d8756494a879734b026
Emon Hossain <emonhossain.me@gmail.com>
2021-11-18 00:01:45 +0600

    auth module edit

--- src/@modules/auth/LoginContainer.tsx
+++ src/@modules/auth/LoginContainer.tsx
@@ -1,36 +1,46 @@
 import AuthFrom from "./elements/AuthFrom";
 import Link from "next/link";
+import Image from "next/image";
 import { Paths } from "@shared/constant/routes.constant";
 // import { useAuth } from "@shared/hooks/useAuth"
 
 export const LoginContainer = () => {
-  // const { loginFn } = useAuth()
-  // if (loginFn.isSuccess) {
-  //     window.location.pathname = '/'
-  // }
-  return (
-    <div className="auth">
-      <div className="login-form">
-        <div className="auth-title">
-          <img className="brand-logo" src="/images/logo.png" alt="logo" />
-          <h1 className="text-3xl font-bold">Log In</h1>
-        </div>
+    // const { loginFn } = useAuth()
+    // if (loginFn.isSuccess) {
+    //     window.location.pathname = '/'
+    // }
+    return (
+        <div className="auth">
+            <div className="login-form">
+                <div className="auth-title">
+                    <Link href={Paths.root}>
+                        <a>
+                            <Image
+                                height={200}
+                                width={200}
+                                src="/images/logo.png"
+                                alt="logo"
+                            />
+                        </a>
+                    </Link>
+                    <h1 className="text-3xl font-bold">Log In</h1>
+                </div>
 
-        <AuthFrom
-          initialValues={{ password: "", phoneNumber: "" }}
-          onFinish={async (val: any) => {
-            // await loginFn.mutateAsync(val)
-          }}
-        />
-        <div className="bottom-link">
-          <span>
-            <b>*</b> Don{"'"}t have an account
-          </span>
-          <Link href={Paths.authRegister}>
-            <a> Register now </a>
-          </Link>
+                <AuthFrom
+                    initialValues={{ password: "", phoneNumber: "" }}
+                    onFinish={async (val: any) => {
+                        // await loginFn.mutateAsync(val)
+                    }}
+                />
+                <div className="bottom-link">
+                    <span>
+                        <b>*</b> Don{"'"}t have an account
+                    </span>
+                    <Link href={Paths.authRegister}>
+                        <a> Register now </a>
+                    </Link>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
--- src/pages/auth/register.tsx
+++ src/pages/auth/register.tsx
@@ -1,29 +1,12 @@
-import AppFooter from "@modules/common/layout/footers/app-footer.component"
-import AppMainHeader from "@modules/common/layout/headers/app-main-header.component"
-import { MoreOutlined } from "@ant-design/icons"
-import { PageHeader } from "antd"
-import PureResponsive from "@modules/common/container/pure-responsive-container.component"
-import { RegisterContainer } from "@modules/auth/RegisterContainer"
+import AppFooter from "@modules/common/layout/footers/app-footer.component";
+import AppMainHeader from "@modules/common/layout/headers/app-main-header.component";
+import { MoreOutlined } from "@ant-design/icons";
+import { PageHeader } from "antd";
+import PureResponsive from "@modules/common/container/pure-responsive-container.component";
+import { RegisterContainer } from "@modules/auth/RegisterContainer";
 
 const Login = () => {
-	return (
-		<>
-			<PureResponsive show="desktop">
-				<AppMainHeader />
-			</PureResponsive>
-			<PureResponsive show="mobile">
-				<PageHeader
-					ghost={false}
-					className="mobile-inner-header"
-					onBack={() => window.history.back()}
-					title="Sign In"
-					extra={[<MoreOutlined key="1" style={{ fontSize: 24 }} />]}
-				/>
-			</PureResponsive>
-			<RegisterContainer />
-			<AppFooter />
-		</>
-	)
-}
+    return <RegisterContainer />;
+};
 
-export default Login
+export default Login;
